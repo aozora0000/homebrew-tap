1c15979f7b324f57c9c79519b0d89c263f3a3c6c
aozora0000 <aozora0000@gmail.com>
2018-12-01 00:08:56 +0900

    feature: update paste-master v0.0.1->v0.0.2

--- paste-master.rb
+++ paste-master.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class PasteMaster < Formula
   homepage "https://github.com/aozora0000/paste-master"
-  url "https://github.com/aozora0000/paste-master/releases/download/v0.0.1/paste-master_0.0.1_Darwin_x86_64.tar.gz"
-  sha256 "5d57bd1113343bcc0751424facbdccf569b3e850fb115a3d16789ac5ad959852"
+  url "https://github.com/aozora0000/paste-master/releases/download/v0.0.2/paste-master_0.0.2_Darwin_x86_64.tar.gz"
+  sha256 "770f2cdeb19a7b5437e9a5dda6d7af1894b2c9b74d66dfb2d10f64542ac5b49c"
   head "https://github.com/aozora0000/paste-master.git"
-  version "0.0.1"
+  version "0.0.2"
 
   def install
     bin.install "paste-master"
