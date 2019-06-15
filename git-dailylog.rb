d12eab52ded79917c116d76f4cb4fa5183c0a7db
aozora0000 <aozora0000@gmail.com>
2019-06-16 02:40:44 +0900

    Update git-dailylog.rb

--- git-dailylog.rb
+++ git-dailylog.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class GitDailylog < Formula
   homepage "https://github.com/aozora0000/git-dailylog"
-  url "https://github.com/aozora0000/git-dailylog/releases/download/v0.4.0/git-dailylog_0.4.0_Darwin_x86_64.tar.gz"
-  sha256 "8fb96fd32a947663ea4f79117f479559ff560122a1fb88d184c01c574d6d1d64"
+  url "https://github.com/aozora0000/git-dailylog/releases/download/v0.5.0/git-dailylog_0.5.0_Darwin_x86_64.tar.gz"
+  sha256 "91fe87be872c14f41b628c8a68ae841cfc5d8a57a79a8248f2b36c735035cdce"
   head "https://github.com/aozora0000/git-dailylog.git"
   version "0.4.0"
 
