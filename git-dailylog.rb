f3840bf7e611c016d7d28c8e4b8e3d61bc9e37a6
aozora0000 <aozora0000@gmail.com>
2019-06-16 02:59:57 +0900

    Update git-dailylog.rb

--- git-dailylog.rb
+++ git-dailylog.rb
@@ -5,7 +5,7 @@ class GitDailylog < Formula
   url "https://github.com/aozora0000/git-dailylog/releases/download/v0.5.0/git-dailylog_0.5.0_Darwin_x86_64.tar.gz"
   sha256 "91fe87be872c14f41b628c8a68ae841cfc5d8a57a79a8248f2b36c735035cdce"
   head "https://github.com/aozora0000/git-dailylog.git"
-  version "0.4.0"
+  version "0.5.0"
 
   def install
     bin.install "git-dailylog"
